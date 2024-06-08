9fc26dfc1a5c29a5be0735630e523283d64d7979
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-06-08 14:32:54 +0530

    project created dynamically

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const ViewAll = () => {
+    const [todos, changeTodos] = useState(
+        { "articles": [] }
+    )
+    const fetchData = () => {
+        axios.get("https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=9b6ac262eea44bcbbf80ae1b064f631d")
+            .then(
+                (response) => {
+                    console.log(response.data)
+                    changeTodos(response.data)
+                }
+            ).catch().finally()
+    }
+    useEffect(() => { fetchData() }, [])
+    return (
+        <div>
+            <h1><center>VIEWALL PRODUCTS</center></h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row">
+                            {
+                                todos.articles.map(
+                                    (value, index) => {
+                                        return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                            <div className="card">
+                                                <img src={value.urlToImage} className="card-img-top" alt="..."></img>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">title:{value.title}</h5>
+                                                    <p className="card-text">description:{value.description}</p>
+                                                    <a href="#" className="btn btn-primary">view details</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    }
+                                )
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
---
+++ src/components/ViewSemi.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const ViewAll = () => {
+    const [todos, changeTodos] = useState([])
+    const fetchData = () => {
+        axios.get("https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=9b6ac262eea44bcbbf80ae1b064f631d")
+        .then(
+            (response) => {
+                changeTodos(response.data)
+            }
+        ).catch().finally()
+    }
+    useEffect(() => { fetchData() }, [])
+    return (
+        <div>
+            <h1><center>VIEWALL PRODUCTS</center></h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row">
+                            {
+                                todos.map(
+                                    (value, index) => {
+                                        return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                            <div className="card">
+                                                <img src={value.image} className="card-img-top" alt="..."></img>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">title:{value.title}</h5>
+                                                    <p className="card-text">price:{value.price}</p>
+                                                    <a href="#" className="btn btn-primary">view details</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    }
+                                )
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
